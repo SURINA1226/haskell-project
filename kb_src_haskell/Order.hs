02d3e513eeeb8ff646aba62113c794e770ec99f1
SURINA1226 <surina1226@gmail.com>
2025-01-03 19:14:07 +0900

    Update Order.hs

--- kb_src_haskell/Order.hs
+++ kb_src_haskell/Order.hs
@@ -1,7 +1,7 @@
 module Order where
 
 import Util
-import TRS
+import TRS 
 
 type Mset a = [a]
 
@@ -49,18 +49,84 @@ grter_symbol ol x y =
 
 
 grtereq_lex :: Ord a => (a -> a -> Bool) -> [a] -> [a] -> Bool
-{- grtereq_lex ..... -}
+grtereq_lex p (x:xs) [] =True
+grtereq_lex p [] y =False
+grtereq_lex p (x:xs) (y:ys)  = 
+    (not((x:xs)==[])&&(y:ys)==[])||p x y ||((x==y )&&grtereq_lex p xs ys) 
 
 grtereq_lpo :: Assoc String Int -> Term -> Term -> Bool
-{- grtereq_lpo ..... -}
+grtereq_lpo t (Node(VSym x,y)) (Node(VSym m,n)) = 
+    if (x==m)then True--eq_symbol t (prsym (VSym x)) (prsym (VSym m))
+    else False
+grtereq_lpo t (Node(x,y)) (Node(m,n))=(matchterm y (Node(m,n))) 
+    ||(eq_symbol t (prsym x) (prsym m)&& (grtereq_lex (grtereq_lpo t) y  n) && xxx t (Node(x,y)) n)
+    ||((grter_symbol t (prsym x) (prsym m))&& xxx t (Node(x,y)) n )
+    ||three t y (Node(m,n))
+    
+
+prterms::[Term]->[String]
+prterms [] =[] 
+prterms (x:xs) =prterm x :prterms xs 
+xxx :: Assoc String Int->Term->[Term]->Bool
+xxx _ _ []=True
+xxx t x (y:ys)=
+    case mkgrter (grtereq_lpo t) x y of 
+        (False)->False
+        (True)->True&& xxx t x ys
+
+three ::Assoc String Int-> [Term]->Term->Bool
+three _ [] _=False
+three t (x:xs) y=
+    case (grtereq_lpo t x y) of
+        True -> True 
+        False->three t xs y
+matchterm :: [Term] -> Term ->Bool
+matchterm [] _ =False
+matchterm (y:ys) p = if y==p then True else matchterm ys p
+
 grter_lpo :: Assoc String Int -> Term -> Term -> Bool
-{- grter_lpo .....   -}
+grter_lpo t x y = mkgrter (grtereq_lpo t) x y
 
 
-grtereq_mset :: Ord a => (a -> a -> Bool) -> Mset a -> Mset a -> Bool
-{- grtereq_mset ..... -}
 
 grtereq_rpo :: Assoc String Int -> Term -> Term -> Bool
-{- grtereq_rpo .....  -}
+grtereq_rpo t (Node(x,y)) (Node(m,n)) = (matchterm y (Node(m,n)))
+    || ((eq_symbol t (prsym x) (prsym m))&&grtereq_mset (grter_symbol [])  y n)
+    ||(not( (eq_symbol t (prsym x) (prsym m)))&& xxx2 t (Node(x,y)) n )
+    ||(three2 t y (Node(m,n)))
+
+xxx2 :: Assoc String Int->Term->[Term]->Bool
+xxx2 _ _ []=True
+xxx2 t x (y:ys)=mkgrter (grtereq_rpo t) x y && xxx t x ys  
+
+three2 ::Assoc String Int-> [Term]->Term->Bool
+three2 _ [] _=False
+three2 t (x:xs) y=if(grtereq_rpo t x y) then True else three t xs y
+
 grter_rpo :: Assoc String Int -> Term -> Term -> Bool
-{- grter_rpo .....    -}
+grter_rpo t x y=mkgrter (grtereq_rpo t) x y
+
+
+grtereq_mset :: Ord a => (a -> a -> Bool) -> [Term] -> [Term] -> Bool
+grtereq_mset p x y= y==[]
+   || case msettest x y of
+        (a,b)->big a b
+
+big ::[Term]->[Term]->Bool
+big a []=True
+big [] b=False
+big (x:xs) (y:ys)=
+    if grter_symbol [("B",0),("A",1),("H",3),("G",3)] (prterm x) (prterm y) then
+        big (x:xs) ys
+    else big xs (y:ys)
+msettest ::[Term]->[Term]->([Term],[Term])
+msettest a []=(a,[]) 
+msettest [] b=([],b) 
+msettest (x:xs) (y:ys)= 
+    if(elem x (y:ys)) then msettest (removeelment x (x:xs)) (removeelment x (y:ys)) 
+    else (x:fst(msettest xs (y:ys)),snd(msettest xs (y:ys)))
+removeelment ::Eq a => a->[a]->[a]
+removeelment _ []=[]
+removeelment x (y:ys)=
+    if(x==y)then ys
+    else y:removeelment x ys 
